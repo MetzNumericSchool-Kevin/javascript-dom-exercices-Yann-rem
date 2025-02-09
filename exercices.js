c85ecdf72f72faf3c1519ca3d8f5e391cb34c684
Yannick Remy <remyyannick@sfr.fr>
2025-02-09 23:57:44 +0100

    Exercice 2 : Des informations manquent !

--- exercices.js
+++ exercices.js
@@ -6,3 +6,18 @@ const titleContent = title.textContent;
 
 // Affichage du titre de la boutique dans la console
 console.log(titleContent);
+
+// Exercice 2 : Des informations manquent !
+
+// Récupération de la description de la boutique
+const shopDescription = document.querySelector("#description_boutique");
+
+// Création d'un nouvel élément paragraphe
+const paragraph = document.createElement("p");
+
+// Ajout de texte à l'élément paragraphe
+paragraph.textContent =
+  "Pour cela, il vous suffit de cliquer sur une des potions de la liste ci-dessous pour l'acheter.";
+
+// Ajout de l'élément paragraphe dans la description de la boutique
+shopDescription.appendChild(paragraph);

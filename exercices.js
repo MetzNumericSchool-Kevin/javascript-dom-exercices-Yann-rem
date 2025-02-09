835fe74054e15c72440d5d32366f36e1ab95b81c
Yannick Remy <remyyannick@sfr.fr>
2025-02-09 23:57:03 +0100

    Exercice 1 : Quel est le titre de la boutique

---
+++ exercices.js
@@ -0,0 +1,8 @@
+// Exercice 1 : Quel est le titre de la boutique
+
+// Récupération du titre de la boutique
+const title = document.querySelector("h1");
+const titleContent = title.textContent;
+
+// Affichage du titre de la boutique dans la console
+console.log(titleContent);
